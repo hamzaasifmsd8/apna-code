73d1fb2881dcd6d1746d1a56614e773730ab22a6
hamzaasifmsd8 <hamzaasifmsd8@gmail.com>
2024-12-16 21:47:20 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+// var gotome = document.querySelectorAll('.hi');
+// gotome.addEventlistner('click',function(){
+//  gotome.classList('me')
+// })
+
+// const chat = document.getElementsByClassName("chat");
+
+// let hello=chat.addEventListner('click',()=>{
+
+//     console.log('Hello world!')
+// });
+
+// console.log(hello);
+
+// console.log("ehllldflkaskfjdsf")
+
+
+
+
